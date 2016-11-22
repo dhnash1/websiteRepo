547826812c9ec0d01c832f13039e37c550a03d98
Deforis Nash <Deforis Nash>
2016-11-22 13:23:13 -0600

    More code

--- skik.js
+++ skik.js
@@ -1,3 +1,9 @@
 function doStuff(x,y){
-  console.log(x,y);
+  console.log("between " + x + " and " + y);
+  if(x.length > y.length){
+    return(x + " is bigger");
+  }
+  else{
+    console.log(y + " is bigger");
+  }
 }
